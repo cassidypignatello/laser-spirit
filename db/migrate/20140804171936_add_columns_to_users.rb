265261bab4b60142a9586bbbf395babc3a3d0044
cassidypignatello <cassidy.pignatello@gmail.com>
2014-08-04 13:19:56 -0400

    adds attributes to user model

---
+++ db/migrate/20140804171936_add_columns_to_users.rb
@@ -0,0 +1,10 @@
+class AddColumnsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :street_1, :string
+    add_column :users, :street_2, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :zip_code, :string
+  end
+end
