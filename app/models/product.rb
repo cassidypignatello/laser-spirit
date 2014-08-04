f40c1159db4c102559bb05af5167bdac3a74eb8c
Zack Sheppard <z@zack.io>
2014-08-04 13:45:00 -0400

    add association syntax fixes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ActiveRecord::Base
+  belongs_to :seller, :class_name => "User" 
+  has_many :line_items
 end
--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -1,4 +1,4 @@
 class ShoppingCart < ActiveRecord::Base
-  belongs_to :buyer class_name: "User"
-  has_many: line_items, dependent: :destroy 
+  belongs_to :buyer, class_name: "User"
+  has_many :line_items, dependent: :destroy 
 end
