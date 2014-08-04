d241c9e0f7efcb717c09639ef80c4f7e69a51d12
dsully360 <dsully15@gmail.com>
2014-08-04 15:55:58 -0400

    Added error handling to new product model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,18 @@ class Product < ActiveRecord::Base
   belongs_to :seller, :class_name => "User" 
   has_many :line_items
   has_many :shopping_carts, through: :line_items
+
+
+  before_destroy :check_for_carts
+  private
+
+  def check_for_carts
+    if line_items.empty?
+      return true
+    else
+      return errors.add(:base, 'This product is is currently being purchased by a buyer!')
+  end
 end
+
+
+
