c5f4f1fdad5148d615ed181891cb54471b6df6ad
biancatompkins <biancatompkins@gmail.com>
2014-08-06 16:01:08 -0400

    adds calculator

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -14,6 +14,38 @@ class ShoppingCart < ActiveRecord::Base
     current_item
   end
 
+#   def consolidate_cart(cart: [])
+#   consolidated_cart={}
+#   cart.each do |item|
+#     item.each do |name, attributes|
+#       if consolidated_cart[name]
+#         consolidated_cart[name][:count]+=1
+#       else
+#         consolidated_cart[name]=attributes
+#         consolidated_cart[name][:count]=1
+#       end
+#     end
+#   end
+#   consolidated_cart
+# end
+
+  def cart_calculator
+    total = 0
+    self.line_items.each do |line|
+     line_total = line.product.price * line.quantity
+     total += line_total
+    end
+    total
+  end
+
+
+
+ 
+
+#get product prices -> line.product.price X line.quantity
+
+#sum all product
+
 
 end
 
