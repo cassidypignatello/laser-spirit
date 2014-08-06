4c049c9c3dc8c832815bc604482fbfe2be0845e4
dsully360 <dsully15@gmail.com>
2014-08-06 14:36:13 -0400

    Working on smart cart.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,14 +1,33 @@
 class LineItemsController < ApplicationController
-  
+
+
   def create
-    cart = current_user.shopping_cart
     product = Product.find(params[:product_id])
-     LineItem.create(
-       :shopping_cart_id => cart.id, 
-       :product_id => product.id
-     )
-    redirect_to shopping_cart_path(cart)
+    @line_item = current_user.shopping_cart.add_product(product.id)
+
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to @line_item.shopping_cart,
+          notice: 'Line item was successfully created.' }
+        format.json { render action: 'show',
+          status: :created, location: @line_item }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @line_item.errors,
+          status: :unprocessable_entity }
+      end
+    end
   end
+  
+  # def create
+  #   cart = current_user.shopping_cart
+  #   @line_item = cart.add_product(params[:product_id])
+  #    LineItem.create(
+  #      :shopping_cart_id => cart.id, 
+  #      :product_id => product.id
+  #    )
+  #   redirect_to shopping_cart_path(cart)
+  # end
 
 
   def destroy
@@ -20,7 +39,7 @@ class LineItemsController < ApplicationController
   private
 
   def line_item_params
-    params.require(:line_item).permit(:product_id)
+    params.require(:line_item).permit(:product_id, :quantity)
   end
 
 end
