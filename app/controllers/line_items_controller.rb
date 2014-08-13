106279990b667384eb7aae1f061d605e03a0de5b
cassidypignatello <cassidy.pignatello@gmail.com>
2014-08-13 11:49:15 -0400

    moves cart image tag into create JS

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -5,7 +5,7 @@ class LineItemsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     @line_item = current_user.shopping_cart.add_product(product.id)
-   
+
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to products_path,
